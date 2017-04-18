fab38ee9275d02d89fdd2e018d724ea0e9996459
André Kroetz Berger <andre@kbsystem.com.br>
2017-04-18 19:10:40 -0300

    Algoritimo Ano Bissexto

---
+++ AnoBissexto.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main() {
+  int ano;
+  printf("\n==========ANO BISSEXTO==========\n");
+  printf("\nDigite o ano: ");
+  scanf("%d", &ano);
+
+  if (ano % 4 == 0 && (ano % 400 == 0 || ano % 100 != 0)) {
+    printf("\nAno bissexto.\n");
+
+  }
+
+  else {
+    printf("\nAno nao bissexto.\n");
+
+  }
+
+}
